c6c5ac183dbbdca50554e4490ee7bc02a57eaa64
ki-hong <rlghd153@gmail.com>
2022-08-22 22:16:00 +0900

    first commit

---
+++ src/main/java/com/codestates/todoapp/controller/TodoController.java
@@ -0,0 +1,14 @@
+package com.codestates.todoapp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @GetMapping("/")
+    public String helloTodo(){
+        return "To-do Application";
+    }
+}
